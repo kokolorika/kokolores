67d986747911e06d0627936d6e7dcd3fc6a65683
Thomas Breitbach <tbreitbach@gmail.com>
2020-02-14 09:50:39 +0100

    Set Access-Control-Allow-*

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var kokolores = require('./kokolores.js')
 
 app.get('/', function (req, res) {
   var kokoloresStatement = kokolores();
+  res.append('Access-Control-Allow-Origin', ['https://kokolorika.github.io/timesheet/']);
+  res.append('Access-Control-Allow-Methods', 'GET');
   console.log(kokoloresStatement);
   res.send({ kokolores: kokoloresStatement });
 })
